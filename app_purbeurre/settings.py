67ce333d845c092b87ced8892f651a257953897e
Flavien <flavien.murail@gmail.com>
2020-07-14 11:05:24 +0200

    Add the email functionality to settings

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -145,5 +145,8 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/authentication"
 
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
