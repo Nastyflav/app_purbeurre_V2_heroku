6cc3a7b6ad24fdf963ad4feff08d75855cf91553
Flavien <flavien.murail@gmail.com>
2020-07-14 10:53:24 +0200

    New secret key

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z*?oU1#],e(x\\\tG]JLN\rcW=g'
+SECRET_KEY = "3_]\x0cxV<Aq-l%S.ha|t\ta'Nbm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
