2ea3980813450cc9e611b0335116102170781558
Flavien <flavien.murail@gmail.com>
2020-07-14 11:38:35 +0200

    Minor changes on frontend

--- static/js/searchbar.js
+++ static/js/searchbar.js
@@ -1,5 +1,5 @@
 function removeChar() {
     var query = document.getElementById("query").value;
     query = query.replace(/[^a-zA-Z0-9]/g, '');
-    document.getElementById("query").value  = query;
+    document.getElementById("query").value = query;
 }
