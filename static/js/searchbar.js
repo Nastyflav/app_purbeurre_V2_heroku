6b2723722e2f20e5df0cd242f0e8b91f59ec7da3
Flavien <flavien.murail@gmail.com>
2020-07-15 08:31:57 +0200

    Improve the bug correction on the frontend side : both of the search forms are working now

--- static/js/searchbar.js
+++ static/js/searchbar.js
@@ -1,5 +1,11 @@
-function removeChar() {
-    var query = document.getElementById("query").value;
+function removeCharNavBar() {
+    var query = document.getElementById("query-navbar").value;
     query = query.replace(/[^a-zA-Z0-9]/g, '');
-    document.getElementById("query").value = query;
+    document.getElementById("query-navbar").value = query;
+}
+
+function removeCharMain() {
+    var query = document.getElementById("query-main").value;
+    query = query.replace(/[^a-zA-Z0-9]/g, '');
+    document.getElementById("query-main").value = query;
 }
