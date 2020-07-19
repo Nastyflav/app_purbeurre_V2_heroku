ca23cb8aac88c4f5642e5713774cdf78a94537ad
Flavien <flavien.murail@gmail.com>
2020-07-19 11:53:32 +0200

    Improved the jquery script to remove the accents from the string

--- static/js/searchbar.js
+++ static/js/searchbar.js
@@ -1,11 +1,11 @@
 function removeCharNavBar() {
     var query = document.getElementById("query-navbar").value;
-    query = query.replace(/[^a-zA-Z0-9]/g, '');
+    query = query.normalize("NFD").replace(/[^a-zA-Z0-9]/g, '');
     document.getElementById("query-navbar").value = query;
 }
 
 function removeCharMain() {
     var query = document.getElementById("query-main").value;
-    query = query.replace(/[^a-zA-Z0-9]/g, '');
+    query = query.normalize("NFD").replace(/[^a-zA-Z0-9]/g, '');
     document.getElementById("query-main").value = query;
 }
