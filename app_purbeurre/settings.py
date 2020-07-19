1ad6ed6456ac24afbc838a02040b8c9559a8fd38
Flavien <flavien.murail@gmail.com>
2020-07-19 12:05:40 +0200

    Added the mail sending functionality

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -145,8 +145,14 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/authentication"
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+# Email sending from a personal Gmail account
+
+EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
