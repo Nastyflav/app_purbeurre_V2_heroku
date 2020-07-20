4baa6665c97b490239ab3dd70a4984b4522f717b
Flavien <flavien.murail@gmail.com>
2020-07-20 11:21:56 +0200

    Try something else to be able the email sending

--- app_purbeurre/settings.py
+++ app_purbeurre/settings.py
@@ -150,10 +150,9 @@ LOGIN_URL = "/authentication"
 EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
